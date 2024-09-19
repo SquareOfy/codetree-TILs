2d4de5e1a4d9a0cddb44bc58efcca01fb06d9d34
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-09-19 15:01:31 +0900

    Title: 술래잡기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240919/술래잡기/hide-and-seek.py
@@ -0,0 +1,181 @@
+"""
+
+1. 도망자 움직이기
+    현재 바라보고 있는 방향으로 1칸 움직일 때
+        격자를 벗어나지 않는 경우
+            술래가 있는 경우라면 움직이지 않습니다.
+            술래 없다면 해당 칸으로 이동.  나무가 있어도 괜찮
+
+        격자를 벗어나는 경우
+            방향을 반대로 틀어주기.
+            바라보고 있는 방향으로 1칸 움직인다 했을 때
+            해당 위치에 술래가 없다면 1칸 앞으로 이동합니다.
+
+2. 술래 움직이기
+    가운데에서 시작해서 상 우 하 좌 순으로 달팽이 모양으로 움직임
+    도달하면 반대로 움직여서 중심으로 오기
+
+    ************주의 !!!!!!!!!!!!!!!!!!!!!111
+    한칸 이동 후 방향을 트는 곳이라면 방향을 바로 틀어준다 !!!!!!! !!
+    ********************************************8
+나무의 역할이 뭐지 ?
+만약 나무가 놓여 있는 칸이라면, 해당 칸에 있는 도망자는 나무에 가려져 보이지 않게 됩니다.
+술래가 보고 있는 방향을 끝까지 보고 나무 있는 칸의 도망자는 살아남고 아니면 잡힌다 !
+
+점수
+    t번째 턴일 때, t*잡힌 도망자 수
+
+
+
+1. 도망자 움직이기
+    도망자 위치 배열에 담아두기 ( arr 배열에는 도망자 번호로 넣어두기!!!)
+    도망자 배열 탐색하며 다음 위치 새 배열에 넣기 (술래와의 거리 3이하인지 확인하고 움직이기, 아니면 그대로)
+        oob 활용
+        oob 아닐 때 술래 위치 idx 로 기억해둔 것 활용
+
+2. 술래 이동
+    달팽이 모양(술래 위치 순서) 배열에 담아두기
+    그 때의 방향 배열 만들어 두기
+
+    술래 현 위치가 끝점이면 위치 index 1 빼기
+    아니면 index +1
+
+    index 변경 시킨 후 방향도 방향 배열의 값으로 갱신해두기
+
+    그리고 그 방향에서 사람 있나 쭈우우욱 보고 잡기 + 점수 더하기
+"""
+def oob(i, j):
+    return i<0 or j<0 or i>=N or j>=N
+
+def get_distance(i, j, x, y):
+    return abs(i-x)+abs(j-y)
+
+#인덱스 1부터 시작하니까 꼭 1 빼기 !!!!!!!!!!!!!!!!!!!!!!!!!!!
+N, M, H, K = map(int, input().split())
+
+arr = [[-1]*N for _ in range(N)]
+tree = [[0]*N for _ in range(N)]
+runner = []
+route = []
+d_by_route = []
+dir = (-1, 0),(0, 1), (1, 0), (0, -1) #상 / 우 / 하 / 좌 (1, 2그대로 유지! )
+answer = 0
+
+#도망자 입력 받기
+for i in range(1, M+1):
+    x, y, d = map(int, input().split())
+    runner.append([x-1, y-1, d])
+    arr[x-1][y-1] = i
+
+for j in range(H):
+    x, y = map(int, input().split())
+    tree[x-1][y-1] = 1
+
+
+#술래 이동 route 배열 만들어 놓기
+r = N//2
+c = N//2
+l = 1
+cnt = 0
+route.append((r,c))
+d_by_route.append(0)
+d = 0
+while 1:
+    di, dj = dir[d]
+    for k in range(l):
+        r += di
+        c += dj
+        route.append((r, c))
+        d_by_route.append(d)
+        if r==0 and c==0:
+            break
+
+    cnt += 1
+    if cnt ==2:
+        l+=1
+        cnt = 0
+    d += 1
+    d %= 4
+    d_by_route[-1] = d
+    if r==0 and c==0:
+        d_by_route[-1] = 2
+        break
+######################달팽이 확인 완
+# print(route)
+# print(d_by_route)
+####################################33
+
+# 1. 도망자 움직이기
+#     도망자 배열 탐색하며 다음 위치 새 배열에 넣기 (술래와의 거리 3이하인지 확인하고 움직이기, 아니면 그대로)
+#         oob 활용
+#         oob 아닐 때 술래 위치 idx 로 기억해둔 것 활용
+#         격자를 벗어나지 않는 경우
+#             술래가 있는 경우라면 움직이지 않습니다.
+#             술래 없다면 해당 칸으로 이동.  나무가 있어도 괜찮
+#
+#         격자를 벗어나는 경우
+#             방향을 반대로 틀어주기.
+#             바라보고 있는 방향으로 1칸 움직인다 했을 때
+#             해당 위치에 술래가 없다면 1칸 앞으로 이동합니다.
+#현재 술래 정보
+idx = 0
+r = 1
+
+for k in range(1, K+1):
+    cr, cc = route[idx]
+    d = d_by_route[idx]
+    new_arr = [[-1]*N for _ in range(N)]
+    new_runner = []
+    for i,j,run_d in runner:
+
+        if arr[i][j] == -1:
+            continue
+        #움직임 가능성 체크
+        dist = get_distance(i, j, cr, cc)
+        if dist>3:
+            new_runner.append([i, j, run_d])
+            new_arr[i][j] = d
+            continue
+        #움직이는 도망자라면
+        di, dj = dir[run_d]
+        du = i+di
+        dv = j+dj
+        if oob(du, dv): #
+            run_d = (run_d+2)%2
+            du -= di*2
+            dv -= dj*2
+        if du == cr and dv == cc: #움직이지 않는다
+            new_arr[i][j] = run_d
+            new_runner.append([i, j, run_d])
+        else: #움직였다
+            new_arr[du][dv] = run_d
+            new_runner.append([du, dv, run_d])
+
+    #옮긴 도망자 원본 배열에 다시 반여하기
+    for i in range(N):
+        arr[i] = new_arr[i][:]
+    runner = new_runner[:]
+
+    #술래 옮기기
+    if idx == N*N -1:
+        r*= -1
+    idx += r
+
+    #술래의 옮긴 위치와 방향
+    cr, cc = route[idx]
+    di, dj = dir[d_by_route[idx]]
+
+    catch = 0
+    while 1:
+        cr += di
+        cc += dj
+        if oob(cr, cc):
+            break
+        if tree[cr][cc]:
+            continue
+        if arr[cr][cc]!=-1:
+            catch +=1
+            arr[cr][cc] = -1
+    answer += k*catch
+
+print(answer)
