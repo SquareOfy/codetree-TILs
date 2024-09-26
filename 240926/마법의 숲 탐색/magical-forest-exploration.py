34dd80acf606a29ecf9fdb4e89b07f58c82fd43d
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-09-26 19:44:46 +0900

    Title: 마법의 숲 탐색, Time: 368ms, Memory: 29MB, Status: Passed - CodeTree

--- 240926/마법의 숲 탐색/magical-forest-exploration.py
+++ 240926/마법의 숲 탐색/magical-forest-exploration.py
@@ -12,30 +12,23 @@
 
 
 def oob(r, c):
-    return r < 0 or c < 0 or r >= R or c >= C
+    return r < 0 or c < 0 or r >= R+3 or c >= C
 
 
 def can_move(r, c, dk):
     if dk == 2:
         if oob(r + 2, c):
             return False
-        if r==-2:
-            if not arr[r+2][c]:
-                return True
         if not arr[r + 2][c] and not arr[r + 1][c - 1] and not arr[r + 1][c + 1]:
             return True
         return False
     elif dk == 3:
-        if r==-2 and arr[r+2][c-1]:
-            return True
         if oob(r, c - 2):
             return False
         if  not arr[r][c - 2] and not arr[r + 1][c - 1] and not arr[r - 1][c - 1]:
             return True
         return False
     elif dk == 1:
-        if r==-2 and arr[r+2][c+1]:
-            return True
         if oob(r, c + 2):
             return False
         if not arr[r][c + 2] and not arr[r + 1][c + 1] and not arr[r - 1][c + 1]:
@@ -59,97 +52,49 @@ def move_to_exit(r, c, d):
     mx = max(r+1, mx)
     return mx
 
-def move(cr, cc, d):
-    while 1:
-        if can_move(cr, cc, 2):  # 남쪽 이동 가능하면
-            cr += 1
-        elif can_move(cr, cc, 3) and can_move(cr, cc - 1, 2):  # 서쪽 이동 가능하면
-            cr += 1
-            cc -= 1
-            d = (d - 1) % 4
-        elif can_move(cr, cc, 1) and can_move(cr, cc + 1, 2):  # 동쪽 이동 가능하면
-            cr += 1
-            cc += 1
-            d = (d + 1) % 4
-        else:  # 다 불가능
-            break
-        if cr == R-2:
-            break
-    return cr, cc, d
 
 R, C, K = map(int, input().split())
-arr = [[0] * C for _ in range(R)]
+arr = [[0] * C for _ in range(R+3)]
 DIR = (-1, 0), (0, 1), (1, 0), (0, -1)
 gol_info = [0]*(K+1)
 answer = 0
 for i in range(1, K + 1):
-    # print(f"+------------------------{i}-------------------------")
-    c, d = map(int, input().split())
-    c-=1
-    # cr = -2
-    # cc -= 1
-    mxr = 0
-    mxc = c
-    ans_d = -1
-    #그냥 다 가보기
-    br = -2
-    # 내자리부터 서쪽까지 가보기
-    for cc in range(c, 0, -1):
-        tr, tc, td = move(-2, cc, d)
-        if br == tr:
+    cc, d = map(int, input().split())
+    cr = 1
+    cc -= 1
+
+    while 1:
+        if can_move(cr, cc, 2):
+            cr += 1
+        elif can_move(cr, cc, 3) and can_move(cr, cc - 1, 2):
+            cr += 1
+            cc -= 1
+            d = (d - 1) % 4
+
+        elif can_move(cr, cc, 1) and can_move(cr, cc + 1, 2):
+            cr += 1
+            cc += 1
+            d = (d + 1) % 4
+        else:
             break
-        if mxr<tr:
-            mxr= tr
-            mxc = tc
-            ans_d = td
+        if cr == R+1:
+            break
 
-    #동쪽 보기
-    if mxr==-1:
-        br = -2
-        for cc in range(c+1, C-1):
-            tr, tc, td = move(-2, cc, d)
-            if br == tr:
-                break
-            if mxr < tr:
-                mxr = tr
-                mxc = tc
-                ans_d = td
-
-    if mxc < c:
-        ans_d = (d-(c-mxc))%4
-    elif mxc>c:
-        ans_d = (d+mxc-c)%4
-    else:
-        ans_d = d
-    if mxr<1:
-        arr = [[0]*C for _ in range(R)]
+    if cr<4:
+        arr = [[0]*C for _ in range(R+3)]
         continue
-    arr[mxr][mxc] = i
+    arr[cr][cc] = i
     for di, dj in DIR:
-        arr[mxr+di][mxc+dj] = i
+        arr[cr+di][cc+dj] = i
 
-    gol_info[i] = (mxr, mxc, ans_d)
-
-
+    gol_info[i] = (cr, cc, d)
     visited = [0]*(K+1)
-    tmp = move_to_exit(mxr, mxc, ans_d)+1
+    tmp = move_to_exit(cr, cc, d)-2
     answer += tmp
+    # print(f"=================={i}======================")
     # print(gol_info)
-    # print("tmp : ," , tmp)
-    # print()
-    # for k in range(R):
+    # for k in range(R+3):
     #     print(arr[k])
-    # print()
+    # print("===========================")
 
 print(answer)
-
-
-
-#  가장 남쪽에 도달해 더이상 이동할 수 없으면 정령은 골렘 내에서 상하좌우 인접한 칸으로 이동
-# 현재 위치하고 있는 골렘의 출구가 다른 골렘과 인접하고 있다면
-# 해당 출구를 통해 다른 골렘으로 이동할 수 있습니다.
-
-# 정령은 갈 수 있는 모든 칸 중 가장 남쪽의 칸으로 이동하고 이동을 완전히 종료
-
-
-# 정령의 최종 위치의 행 번호의 합
