0685f1a4f8b98832c3d7d27238d1f39d7f6f9425
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-10-11 11:06:27 +0900

    Title: 미로 타워 디펜스, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241011/미로 타워 디펜스/maze-tower-defense.py
+++ 241011/미로 타워 디펜스/maze-tower-defense.py
@@ -1,14 +1,87 @@
-def set_route_lst():
-    r, c = N // 2, N // 2
-    l = 1
-    cnt = 0
+def oob(i, j):
+    return i < 0 or j < 0 or i >= N or j >= N
 
+
+N, M = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+answer = 0
+DIR = (0, 1), (1, 0), (0, -1), (-1, 0)
+
+
+def set_lst():
+    r, c = N // 2, N // 2
+    cnt = 0
+    l = 1
+    result_lst = []
     while 1:
         for di, dj in (0, -1), (1, 0), (0, 1), (-1, 0):
             for t in range(l):
                 r += di
                 c += dj
-                route_lst.append((r, c))
+
+                if arr[r][c] != 0:
+                    result_lst.append(arr[r][c])
+                if r == 0 and c == 0:
+                    return result_lst
+            cnt += 1
+            if cnt == 2:
+                l += 1
+                cnt = 0
+    return result_lst
+
+
+def delete_continuous(lst):
+    global answer
+    result_lst = []
+    tmp_lst = []
+    bf = -1
+    flag = False
+    for i in range(len(lst)):
+        if lst[i] != bf:
+            if len(tmp_lst) < 4:
+                result_lst.extend(tmp_lst)
+            else:
+                answer += bf * len(tmp_lst)
+                flag = True
+            tmp_lst = [lst[i]]
+            bf = lst[i]
+        else:
+            tmp_lst.append(lst[i])
+    result_lst.extend(tmp_lst)
+    return result_lst, flag
+
+
+def make_new_lst(lst):
+    result_lst = []
+    tmp_lst = []
+    bf = -1
+    for i in range(len(lst)):
+        if bf == lst[i]:
+            tmp_lst.append(lst[i])
+        else:
+            if bf!=-1: result_lst.extend([len(tmp_lst), bf])
+            tmp_lst = [lst[i]]
+            bf = lst[i]
+
+    if bf!=-1: result_lst.extend([len(tmp_lst), bf])
+    return result_lst
+
+
+def set_arr(lst):
+    r, c = N // 2, N // 2
+    cnt = 0
+    l = 1
+    idx = 0
+    while 1:
+        for di, dj in (0, -1), (1, 0), (0, 1), (-1, 0):
+            for t in range(l):
+                r += di
+                c += dj
+                arr[r][c] = lst[idx]
+                idx += 1
+                if idx == len(lst):
+                    return
                 if r == 0 and c == 0:
                     return
             cnt += 1
@@ -16,120 +89,43 @@ def set_route_lst():
                 l += 1
                 cnt = 0
 
-def pull():
-    k = 0
-    while k<N*N-1:
-        r, c = route_lst[k]
-        if arr[r][c]!=0:
-            k+=1
-            continue
-        idx = k
-        du, dv = r, c
-        while arr[du][dv]==0 :
-
-            idx += 1
-            if idx>=N*N-1: break
-            du, dv = route_lst[idx]
-
-        if idx==N*N-1:
-            break
-        arr[r][c] = arr[du][dv]
-        arr[du][dv] = 0
-
-
-
-def oob(r, c):
-    return r < 0 or c < 0 or r >= N or c >= N
-
-
-def printa(string, arr):
-    print(f"====================={string} ===================")
-    for i in range(N):
-        print(arr[i])
-    print("=================================================")
-    print()
-
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-route_lst = []
-DIR = (0, 1), (1, 0), (0, -1), (-1, 0)
-C_r, C_c = N // 2, N // 2
-answer = 0
-# print(route_lst)
-
-set_route_lst()
-
-
-def find_continuous(i):
-    r, c = route_lst[i]
-    num = arr[r][c]
-    cnt = 1
-    idx = i+1
-    while idx<N*N-1:
-        nr, nc = route_lst[idx]
-        if arr[nr][nc] != num:
-            break
-        cnt+=1
-        idx+=1
-    return cnt
 
 for m in range(M):
-    d, s = map(int, input().split())
-
-    tr, tc = N // 2, N // 2
+    d, p = map(int, input().split())
     di, dj = DIR[d]
-    for t in range(1, s+1):
-        tr += di
-        tc += dj
-        if oob(tr, tc): break
-        answer += arr[tr][tc]
-        arr[tr][tc] = 0
-    # printa("공격 후 ", arr)
-    #당기기
-    pull()
-    # printa("당긴 후 ", arr)
+    r, c = N // 2, N // 2
+    for s in range(p):
+        r += di
+        c += dj
+        if oob(r, c): break
+        answer += arr[r][c]
+        arr[r][c] = 0
+    # 0제외한 숫자 목록
+    lst = set_lst()
+    # print("공격 후 당긴 모습")
+    # print(lst)
+    # print("==================================")
+    # print()
 
-
+    # lst에서 연속인 수 제거하고 당기기
     while 1:
-        flag = False
-        #4개 이상인지 개수 찾기
-        for i in range(N*N -1):
-            r, c = route_lst[i]
-            if arr[r][c] == 0: break
-            cnt = find_continuous(i)
-            if cnt<4: continue
-            flag = True
-            answer += (cnt * arr[r][c])
-            for j in range(i, i+cnt):
-                du, dv = route_lst[j]
-                arr[du][dv] = 0
+        lst, flag = delete_continuous(lst)
         if not flag:
             break
-        pull()
 
-    # printa("제거 후 ", arr)
+    # print("연쇄작용 이후 모습")
+    # print(lst)
+    #
+    # print("==========================")
+    # print()
+    # 같은 수 체크해서 새로 lst만들기
+    lst = make_new_lst(lst)
+    arr = [[0] * N for _ in range(N)]
+    set_arr(lst)
 
-    lst = []
-    pointer = 0
-    while 1:
-        r, c = route_lst[pointer]
-        num = arr[r][c]
-        if num==0:
-            break
-        cnt = find_continuous(pointer)
-        lst.extend([cnt, num])
-        if len(lst) >= N*N-1:
-            lst = lst[:N*N-1]
-            break
-        pointer += cnt
-        if pointer>=N*N-1:
-            break
-
-    while len(lst)<N*N-1:
-        lst.append(0)
-
-    for i in range(N*N-1):
-        r, c = route_lst[i]
-        arr[r][c] = lst[i]
-    # printa("완료 후 ", arr)
+    # print("새로운 리스트")
+    # print(lst)
+    #
+    # print("==========================")
+    # print()
 print(answer)
